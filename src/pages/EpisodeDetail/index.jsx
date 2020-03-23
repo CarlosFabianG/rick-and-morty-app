3500926783f37e64c089cc58ea6c692cf71370b0
Carlos Fabián <carl.fab.g@gmail.com>
2020-03-23 01:52:59 -0600

    se hicieron cambios en EpisodeDetail

---
+++ src/pages/EpisodeDetail/index.jsx
@@ -0,0 +1,13 @@
+import react, { Component } from 'react'
+import { Stack } from "@chakra-ui/core"
+
+
+class EpisodeDetail extends Component{
+    state = {}
+
+    render(){
+
+    }
+}
+
+export default EpisodeDetail
