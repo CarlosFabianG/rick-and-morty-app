5306b93df0b64d7b8a31cd01d193f693133c0f2b
Carlos Fabian <carl.fab.g@gmail.com>
2020-08-28 22:36:31 -0500

    done

--- src/store/store.js
+++ src/store/store.js
@@ -17,4 +17,4 @@ export const store = createStore(
                     composeEnhancers(
                         applyMiddleware(thunk)
                         )
-                )
+                    )
